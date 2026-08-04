9e8612d98520191a9b5a179a575f55b5c40b3ceb
Vinendder <vinenddersingh@gmail.com>
2026-08-04 10:17:18 +0530

    New practice ground

---
+++ learning_c/first.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+
+ /*
+ %d - integer
+ %f - float
+ %c - character (char)
+ %lf - double (%f works for double in output but input requires %lf )
+ %s - string/array [eg. char name[] = "Dubs"]
+ 
+ 
+*/
+
+// arthimetic operators: = + - * / % ++ --
+
+int x = 2;
+int y = 3;
+int z = 0;
+
+z = x + y;
+
+printf("%d\n", z);
+
+
+}
