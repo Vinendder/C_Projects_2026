d751f89e84e3ac59a517003aa19402d4e823e887
Vinendder <vinenddersingh@gmail.com>
2026-08-05 11:55:52 +0530

    Mad libs game, entire code made by yours truly (not claude), game demo from bro code -> immediately coding by me -> checking for any QoL changes (none)

--- learning_c/first.c
+++ learning_c/first.c
@@ -3,26 +3,38 @@
 
 int main(){
 
-char item_name[50] = "";
-float price_item = 0.0f;
-int number_item = 0;
-float total_price = 0;
+    // All the variables
+    char adjective_1[50] = "";
+    char adjective_2[50] = "";
+    char adjective_3[50] = "";
+    char noun[50] = "";
+    char verb[50] = "";
 
-printf("What item would you like to buy?: ");
-fgets(item_name, sizeof(item_name), stdin);
-item_name[strcspn(item_name, "\n")] = '\0';
+    // All user inputs
+    printf("Enter an adjective (description): ");
+    fgets(adjective_1, sizeof(adjective_1), stdin);
+    adjective_1[strlen(adjective_1) - 1] = '\0';
 
-printf("What is the price of item?: ");
-scanf("%f", &price_item);
+    printf("Enter a noun (animal of a person): ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0';
 
-printf("What is the quantity of item?: ");
-scanf("%d", &number_item);
+    printf("Enter an adjective (description): ");
+    fgets(adjective_2, sizeof(adjective_2), stdin);
+    adjective_2[strlen(adjective_2) - 1] = '\0';
 
-total_price = price_item * number_item;
+    printf("Enter a verb (ending with -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) -1] = '\0';
 
-printf("Your total bill for %d %s is %.2f\n", number_item, item_name, total_price);
-printf("Thank you for your purchase!\n");
+    printf("Enter an adjective (description): ");
+    fgets(adjective_3, sizeof(adjective_3), stdin);
+    adjective_3[strlen(adjective_3) - 1] ='\0';
+
+    //Final print statement
+    printf("\nToday I went to a %s adoption centre.\nThere I saw %s.\n%s was %s and %s inside the cage.\nEveryone around there was %s. ", adjective_1, noun, noun, adjective_2, verb, adjective_3);
 
 
+    return 0;
 
 }
