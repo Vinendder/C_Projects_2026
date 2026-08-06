f98f561f09ca34f4aae32d915da985008fde4734
Vinendder <vinenddersingh@gmail.com>
2026-08-06 13:27:32 +0530

    Weight converter

--- learning_c/first.c
+++ learning_c/first.c
@@ -3,36 +3,32 @@
 
 int main(){
 
-//All Variables
-double final_amount = 0.0;
-double principal_amount = 0.0;
-double rate_interest = 0.0;
-double compound_interest = 0.0;
-int years = 0;
-int num_compounds = 0;
+    // Variables used
+    int option = 0;
+    float pounds = 0.0f;
+    float kilograms = 0.0f;
 
-// All user inputs
-printf("Enter Initial Investment: ");
-scanf("%lf", &principal_amount);
+    // Choices
+    printf("Choose between the following conversions: \n");
+    printf("1. Kilograms to pounds\n");
+    printf("2. Pounds to kilograms\n");
+    printf("Type 1 or 2.\n");
+    scanf("%d", &option);
 
-printf("Enter rate of interest: ");
-scanf("%lf", &rate_interest);
-rate_interest = rate_interest / 100;
-
-printf("Enter number of years: ");
-scanf("%d", &years);
-
-printf("Enter number of compounds per year: ");
-scanf("%d", &num_compounds);
-
-// Calculations 
-double ToPower = 1 + (rate_interest / num_compounds);
-double PowerValue = pow(ToPower, (num_compounds * years));
-final_amount = principal_amount * PowerValue;
-compound_interest = final_amount - principal_amount;
-
-// Output
-printf("After %d years, your final amount will be: %.2lf\n", years, final_amount);
-printf("Your total compound interest is: %.2lf\n", compound_interest);
-
+    // Calculation on choice
+    if(option == 1){
+        printf("Type weight in kilograms: \n");
+        scanf("%f", &kilograms);
+        pounds = kilograms * 2.20462;
+        printf("The weight in pounds is: %.2f lbs\n", pounds);
+    }
+    else if(option ==2){
+        printf("Type weight in pounds: \n");
+        scanf("%f", &pounds);
+        kilograms = pounds / 2.20462;
+        printf("The weight in pounds is: %.2f kgs\n", kilograms);
+    }
+    else{
+        printf("Type valid option. (Option 1 or 2)\n"); // Guard code
+    }
 }
