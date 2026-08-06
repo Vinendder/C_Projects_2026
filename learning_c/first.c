d575995856efcb610fc0a2493bdb1eda36e54895
Vinendder <vinenddersingh@gmail.com>
2026-08-06 14:08:33 +0530

    Temperature Converter

--- learning_c/first.c
+++ learning_c/first.c
@@ -2,33 +2,34 @@
 #include <math.h>
 
 int main(){
+    
+    // Variables for Temperature
+    char choice = '\0';
+    float Fahrenheit = 0.0f;
+    float Celsius = 0.0f;
 
-    // Variables used
-    int option = 0;
-    float pounds = 0.0f;
-    float kilograms = 0.0f;
+    // User input
+    printf("Choose one conversion between the following: \n");
+    printf("C. Fahrenheit to Celsius.\n");
+    printf("F. Celsius to Fahrenheit.\n");
+    printf("Type 'C' or 'F' \n");
+    scanf(" %c", &choice);
 
-    // Choices
-    printf("Choose between the following conversions: \n");
-    printf("1. Kilograms to pounds\n");
-    printf("2. Pounds to kilograms\n");
-    printf("Type 1 or 2.\n");
-    scanf("%d", &option);
-
-    // Calculation on choice
-    if(option == 1){
-        printf("Type weight in kilograms: \n");
-        scanf("%f", &kilograms);
-        pounds = kilograms * 2.20462;
-        printf("The weight in pounds is: %.2f lbs\n", pounds);
+    // Conversion Based on Choice
+    if(choice == 'C'){
+        printf("Type the temperature in Fahrenheit: \n");
+        scanf("%f", &Fahrenheit);
+        Celsius = (Fahrenheit - 32) * (5.0f/9);
+        printf("The temperature in Celsius is %.1f °C\n", Celsius);
     }
-    else if(option ==2){
-        printf("Type weight in pounds: \n");
-        scanf("%f", &pounds);
-        kilograms = pounds / 2.20462;
-        printf("The weight in pounds is: %.2f kgs\n", kilograms);
+    else if(choice == 'F'){
+        printf("Type the temperature in Celsius: \n");
+        scanf("%f", &Celsius);
+        Fahrenheit = (Celsius * (9.0f/5) + 32);
+        printf("The temperature in Fahrenheit is %.1f °F\n", Fahrenheit);
     }
     else{
-        printf("Type valid option. (Option 1 or 2)\n"); // Guard code
+        printf("Type Valid Choice (C or F)\n"); // Guard code
     }
+
 }
