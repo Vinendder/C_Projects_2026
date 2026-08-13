740aabd2d6fab6d1ed59e6ef2ffc0b805b797364
Vinendder <vinenddersingh@gmail.com>
2026-08-13 14:44:32 +0530

    Added comments

--- BankingProgram/bp.c
+++ BankingProgram/bp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Functions used
 void CheckBalance(float AccBalance);
 float Withdraw(float AccBalance);
 float Deposit(float AccBalance);
@@ -10,24 +11,24 @@ int main(){
     int OnOff = 1;
     float AccBalance = 0.0f;
 
-    do{
+    do{ // Main loop
         int choice = 0;
 
-        do{
+        do{ // Secondary loop to check user input
             printf("What do you want to do?:\n");
             printf("1. Check Balance\n");
             printf("2. Deposit Money\n");
             printf("3. Withdraw Money\n");
             printf("0. Exit Program\n");
     
-            if (scanf("%d", &choice) != 1){
+            if (scanf("%d", &choice) != 1){ // guard for abc
                 while(getchar() != '\n');
                 choice = 0;
             }
     
         }while(choice < 0 || choice > 3);
 
-        switch(choice){
+        switch(choice){ // Matching user input with functions
             case 1: 
                 CheckBalance(AccBalance);
                 break;
@@ -49,22 +50,22 @@ int main(){
     return 0;
 
 }
-void CheckBalance(float AccBalance){
+void CheckBalance(float AccBalance){  // Check balance 
     float Balance = AccBalance;
     printf("Your Balance is: %.2f\n", Balance);
 }
 
-float Deposit(float AccBalance){
+float Deposit(float AccBalance){ // Deposit Money
     float money;
 
     printf("Enter the amount which you want to deposit: ");
-    if(scanf("%f", &money) != 1){
+    if(scanf("%f", &money) != 1){ 
         while(getchar() != '\n');
         printf("Invalid Input\n");
         return AccBalance;
     }
 
-    if(money <= 0){
+    if(money <= 0){ // Guard code
         printf("Invalid Amount\n");
     }
     else{
@@ -73,7 +74,7 @@ float Deposit(float AccBalance){
     return AccBalance;
 }
 
-float Withdraw(float AccBalance){
+float Withdraw(float AccBalance){ // Withdraw money
     float money = 0;
 
     printf("Enter the amount which you want to withdraw: ");
@@ -83,7 +84,7 @@ float Withdraw(float AccBalance){
         return AccBalance;
     }
 
-    if(money > AccBalance){
+    if(money > AccBalance){ // Guard code
         printf("Insufficent Funds!\n");
     }
     else if(money <= 0){
